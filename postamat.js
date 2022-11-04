68374f6005df51e8ec2816a0ae315396847c1143
OksanaTarun <oxana.tarun@gmail.com>
2022-11-04 22:50:13 +0300

    postamat

---
+++ postamat.js
@@ -0,0 +1,9 @@
+let postamat = [1, 2, 3, 4, 5];
+let accountSum = 100;
+let smsCode = "A001DFX0";
+if (smsCode == "A001DFX0") {
+  accountSum -= 100;
+  console.log(
+    `Вы получили посылку из ячейки ${postamat[3]} и ваш счет составляет: ${accountSum} р.`
+  );
+}
