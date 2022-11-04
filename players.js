2f9ff2c327426649692abb2a09170a8da0d17c25
OksanaTarun <oxana.tarun@gmail.com>
2022-11-04 23:30:28 +0300

    players

---
+++ players.js
@@ -0,0 +1,31 @@
+const players = [
+    {
+        "id": 1,
+        "name": "Ivan",
+        "scorePoints": 4500
+     },
+    {
+        "id": 2,
+        "name": "Petr",
+        "scorePoints": 3600
+    },
+    {
+        "id": 3,
+        "name": "Vadim",
+        "scorePoints": 3433
+    },
+    {
+        "id": 4,
+        "name": "Olga",
+        "scorePoints": 2356
+    },
+    
+    ];
+
+    console.log(players);
+    let maxScorePoints = 0;
+    for (let player of players) {
+     maxScorePoints = Math.max(maxScorePoints, player.scorePoints);
+    };
+     
+    console.log(`Максимальное количество очков - ${maxScorePoints}`);
